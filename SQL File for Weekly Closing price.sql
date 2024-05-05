4162003d72b5ac0e241696d33cbf08220ec04176
Sivashankaran <54579777+itsmesivaa@users.noreply.github.com>
2024-05-05 16:28:44 +0530

    Stan Weintein 30Week Moving Average
    Stan Weintein 30Week Moving Average & Price Difference Calculation

--- SQL File for Weekly Closing price.sql
+++ SQL File for Weekly Closing price.sql
@@ -15,8 +15,7 @@ max(high) over (partition by datepart(year,[date]), datepart(WEEK,[date]) ) as w
 min(low) over (partition by datepart(year,[date]), datepart(WEEK,[date]) ) as week_low,
 first_value([close]) over (partition by datepart(year,[date]),  datepart(WEEK,[date]) order by datepart(weekday,[date]) desc) as week_close
 --,[open], [high],[low],[close]
-From stock_ohlc_data where stock = 'HEROMOTOCO'
-
+From stock_ohlc_data where stock in('TITAN')
 group by  
 stock,[date],
 [high],[low],[open],[close]
@@ -28,11 +27,15 @@ datepart(WEEK,[date]) desc
 
 --select * From test_CTE
 
-select distinct stock,
+
+--Calculating Weeking Closing Average and Weekly Stock Closing Price difference with Moving Average
+select distinct top 1 stock,
 current_year, 
 current_week_of_year,
 week_open, week_high, week_low, week_close,
-round(avg(week_close) over (order by current_year,current_week_of_year ROWS between 29 PRECEDING and current row),2) as weekly_closing_average
+round(avg(week_close) over (order by current_year,current_week_of_year ROWS between 29 PRECEDING and current row),2) as weekly_closing_average,
+(week_close - round(avg(week_close) over (order by current_year,current_week_of_year ROWS between 29 PRECEDING and current row),2)) / 
+round(avg(week_close) over (order by current_year,current_week_of_year ROWS between 29 PRECEDING and current row),2) * 100 as PCTDIFF 
 from weekly_closing
 order by current_year desc, current_week_of_year desc
 
