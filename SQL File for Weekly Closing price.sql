1ec3c8b325b887c0ca3e1e1c2a010d4e35ddd368
Sivashankaran <54579777+itsmesivaa@users.noreply.github.com>
2024-05-06 09:39:36 +0530

    30Week SMA & Percentage difference
    Stan Weinstein 30Week SMA & Percentage difference

--- SQL File for Weekly Closing price.sql
+++ SQL File for Weekly Closing price.sql
@@ -6,38 +6,37 @@ WITH weekly_closing_calc
 AS (
 	SELECT DISTINCT stock
 		,
-		--[date],
-		datepart(year, [date]) AS current_year
-		,datepart(WEEK, [date]) AS current_week_of_year
+		max(convert(int,datepart(year, [date]))) AS current_year
+		,max(convert(int,datepart(WEEK, [date]))) AS current_week_of_year
 		,first_value([open]) OVER (
-			PARTITION BY datepart(year, [date])
+			PARTITION BY stock, datepart(year, [date])
 			,datepart(WEEK, [date]) ORDER BY datepart(weekday, [date])
 			) AS week_open
 		,max(high) OVER (
-			PARTITION BY datepart(year, [date])
+			PARTITION BY stock, datepart(year, [date])
 			,datepart(WEEK, [date])
 			) AS week_high
 		,min(low) OVER (
-			PARTITION BY datepart(year, [date])
+			PARTITION BY stock, datepart(year, [date])
 			,datepart(WEEK, [date])
 			) AS week_low
 		,first_value([close]) OVER (
-			PARTITION BY datepart(year, [date])
+			PARTITION BY stock, datepart(year, [date])
 			,datepart(WEEK, [date]) ORDER BY datepart(weekday, [date]) DESC
 			) AS week_close
-	--,[open], [high],[low],[close]
 	FROM stock_ohlc_data
-	WHERE stock IN ('TITAN')
+	--where stock  = 'SBILIFE'
 	GROUP BY stock
 		,[date]
 		,[high]
 		,[low]
 		,[open]
 		,[close]
+
 	)
 
 --Calculating Weeking Closing Average and Weekly Stock Closing Price difference with Moving Average
-SELECT DISTINCT TOP 1 stock
+SELECT stock
 	,current_year
 	,current_week_of_year
 	,week_open
@@ -45,17 +44,20 @@ SELECT DISTINCT TOP 1 stock
 	,week_low
 	,week_close
 	,round(avg(week_close) OVER (
+			PARTITION BY stock
 			ORDER BY current_year
 				,current_week_of_year ROWS BETWEEN 29 PRECEDING
 					AND CURRENT row
 			), 2) AS [30_week_SMA]
 	,(
 		week_close - round(avg(week_close) OVER (
+				PARTITION BY stock 
 				ORDER BY current_year
 					,current_week_of_year ROWS BETWEEN 29 PRECEDING
 						AND CURRENT row
 				), 2)
 		) / round(avg(week_close) OVER (
+			PARTITION BY stock 
 			ORDER BY current_year
 				,current_week_of_year ROWS BETWEEN 29 PRECEDING
 					AND CURRENT row
@@ -63,3 +65,4 @@ SELECT DISTINCT TOP 1 stock
 FROM weekly_closing_calc
 ORDER BY current_year DESC
 	,current_week_of_year DESC
+	,stock asc
