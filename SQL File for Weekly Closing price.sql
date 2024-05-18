2ea016e5e6cc5c86f39ee3a8286d0631eb99fe14
Sivashankaran <54579777+itsmesivaa@users.noreply.github.com>
2024-05-18 18:40:38 +0530

    SQL Query to pull data STAN Weinstein Method 30 Week Moving Average
    SQL Query to pull data STAN Weinstein Method 30 Week Moving Average

--- SQL File for Weekly Closing price.sql
+++ SQL File for Weekly Closing price.sql
@@ -68,25 +68,46 @@ AS (
 				), 2) * 100 AS pct_diff
 	FROM weekly_price_closing_calc_CTE
 	)
+,
+
+Rangebound_6Months AS (
+    SELECT stock,
+           current_year,
+           current_week_of_year,
+		   week_open,
+		   week_high,
+		   week_low,
+           week_close,
+           [30_week_SMA],
+           pct_diff,
+		   ((week_high - week_low) / week_low) * 100 AS WEEKLY_ATR_PCT_RANGE,
+           COUNT(CASE WHEN week_close > [30_week_SMA] THEN 1 END) OVER (
+                PARTITION BY stock
+                ORDER BY current_year, current_week_of_year
+                ROWS BETWEEN 12 PRECEDING AND CURRENT ROW
+           ) AS consecutive_weeks_above_SMA
+    FROM STANWEINSTEIN_3OWEEKSMA_PCT_DIFF
+)
+
+SELECT r6m.stock AS 'STOCKNAME',
+       r6m.current_year AS 'CURRENTYEAR',
+       r6m.current_week_of_year AS 'CURRENTWEEK_OF_YEAR',
+	   r6m.consecutive_weeks_above_SMA AS 'CONSECUTIVE_WEEKS_Above_SMA',
+       r6m.week_open AS 'WEEK_OPEN',
+       r6m.week_high AS 'WEEK_HIGH',
+       r6m.week_low AS 'WEEK_LOW',
+       r6m.week_close AS 'WEEK_CLOSE',
+       r6m.[30_week_SMA] AS '30_WEEK_SMA',
+       ROUND(r6m.pct_diff, 2) AS '30_WEEK_PCT_DIFF',
+	   ROUND(r6m.WEEKLY_ATR_PCT_RANGE,2) AS 'WEEKLY_ATR_PCT_RANGE'
+FROM Rangebound_6Months r6m
+WHERE 
+--r6m.week_close < r6m.[30_week_SMA]
+	  r6m.pct_diff BETWEEN -3.5 AND 3.5
+      AND r6m.consecutive_weeks_above_SMA >= 6
+      AND r6m.current_year = YEAR(GETDATE())
+      AND r6m.current_week_of_year = DATEPART(WEEK, GETDATE())
+ORDER BY r6m.stock ASC,r6m.current_year DESC, r6m.consecutive_weeks_above_SMA DESC,
+r6m.current_week_of_year DESC, r6m.[PCT_DIFF] ASC, r6m.WEEKLY_ATR_PCT_RANGE ASC;
 
 
---Query to filter the stocks Weekly Closing price greater than 30 Week Moving Average as per Stan Weinstein Trading Strategy
-
-SELECT stan.stock AS 'STOCKNAME'
-	,stan.current_year AS 'CURRENTYEAR'
-	,stan.current_week_of_year AS 'CURRENTWEEK_OF_YEAR'
-	,stan.week_open 'WEEK_OPEN'
-	,stan.week_high AS 'WEEK_HIGH'
-	,stan.week_low AS 'WEEK_LOW'
-	,stan.week_close AS 'WEEK_CLOSE'
-	,stan.[30_week_SMA] AS '30_WEEK_SMA'
-	,round(stan.pct_diff, 2) AS 'PCT_DIFF'
-FROM STANWEINSTEIN_3OWEEKSMA_PCT_DIFF stan
-WHERE stan.week_close >= stan.[30_week_SMA]
-	AND stan.[pct_diff] BETWEEN 0
-		AND 8
-	AND current_year = year(getdate())
-	AND current_week_of_year = datepart(week, getdate()) - 1
-ORDER BY stan.current_year DESC
-	,stan.current_week_of_year DESC
-	,stan.[PCT_DIFF] DESC
